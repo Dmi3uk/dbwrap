a4b9a5b4eeefd08650b5fd7d4018c6c32bca37a0
dmi3uk <Dmi3uk@yandex.ru>
2023-02-08 17:00:57 +0200

    init

---
+++ src/main/java/com/daky/registerclientservice/dbwrap/DbwrapApplication.java
@@ -0,0 +1,13 @@
+package com.daky.registerclientservice.dbwrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbwrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbwrapApplication.class, args);
+	}
+
+}
